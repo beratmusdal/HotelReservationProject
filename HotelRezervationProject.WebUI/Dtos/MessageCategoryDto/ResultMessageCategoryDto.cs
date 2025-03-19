e2c218989962f650a04083356f200f54f9a0402f
beratmusdal <beratmsdl@gmail.clm>
2025-03-19 16:20:37 +0300

    commit

---
+++ HotelRezervationProject.WebUI/Dtos/MessageCategoryDto/ResultMessageCategoryDto.cs
@@ -0,0 +1,8 @@
+﻿namespace HotelReservationProject.WebUI.Dtos.MessageCategoryDto
+{
+    public class ResultMessageCategoryDto
+    {
+        public int MessageCategoryID { get; set; }
+        public string MessageCategoryName { get; set; }
+    }
+}
